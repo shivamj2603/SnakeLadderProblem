91d4075f185db383789751cc40c98d43a11c1098
shivamj2603 <shivamj2603@gmail.com>
2020-09-27 21:40:45 +0530

    Option to check for Snake/Ladder/NoPlay added

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,13 +2,37 @@ public class SnakeLadder {
 
 		public static void main(String[] args)
 		{      
+
+
+                       //constants
+                       final int NO_PLAY=0;
+		       final int LADDER=1;
+		       final int SNAKE=2;   
                         
 
                        //variables
 		       int position=0;
 		       int countOfDieRoll=0;
                        int die=0;
+                       int option=0;
+
+
                        die = (int) ( Math.floor(Math.random()*6) + 1);
+                       option=(int) Math.floor(Math.random()*3);
+		            switch(option) {
+		                
+		                case NO_PLAY:
+		                break;
+		                
+		                case LADDER:
+		                position=position + die;
+		                break;
+		                
+		                case SNAKE:
+		                position=position - die;
+		                break;
+                            
+                            }
              
 		}
 
